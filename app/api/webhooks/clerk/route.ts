33f8924e913ab8d92bcb46005c6f2c0a1c06bf31
shamaskaramat <shams4195140@gmail.com>
2024-11-26 18:37:41 +0500

    vercel

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: Request) {
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-
+    console.log(id)
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
